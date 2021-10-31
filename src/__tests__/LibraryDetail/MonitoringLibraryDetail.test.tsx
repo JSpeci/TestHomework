2ae2c061a3d5d33cddc45a447f9c7c99776097ad
codewaseem <codewaseem@gmail.com>
2021-10-31 19:10:40 +0530

    add empty/skipped tests on appearance for Monitoring Library Detail

---
+++ src/__tests__/LibraryDetail/MonitoringLibraryDetail.test.tsx
@@ -0,0 +1,10 @@
+// Write empty/skipped tests on appearance for Monitoring Library Detail
+
+describe("Library Detail", () => {
+  it.skip("click on a library opens library details page", () => {});
+  it.skip("libary name and libary note is displayed", () => {});
+  it.skip("details of monitoring questions in the selected library are shown", () => {});
+  it.skip("can delete the library ", () => {});
+});
+
+export {};
